86553384549ce3d5a295764a13b28023d551e9cc
Ashrith Sagar <ashrithy@iisc.ac.in>
2026-02-03 03:37:40 +0530

    ➕ PLY:pacer

--- playground/PACER/src/pacer/__init__.py
+++ playground/PACER/src/pacer/__init__.py
@@ -5,6 +5,9 @@ Progress-Aligned Curation for Error-Resilient Imitation Learning
 """
 # src/pacer/__init__.py
 
+from rich.console import Console
 from rich.traceback import install
 
 install()
+
+console = Console()
