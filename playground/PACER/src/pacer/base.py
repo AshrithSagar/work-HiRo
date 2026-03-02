ea463b99413f312ad2c631084eade12bb8368644
Ashrith Sagar <ashrithy@iisc.ac.in>
2026-03-03 01:53:18 +0530

    🚑️ PLY:pacer

--- playground/PACER/src/pacer/base.py
+++ playground/PACER/src/pacer/base.py
@@ -622,30 +622,33 @@ class PACER(Generic[NumBins, NumDemos, NumPoints, DimState, DimAction]):
             N, Residuals[NumPoints]()
         )
 
-        # (N,)
-        MAD_residuals = Residuals[NumDemos]()
+        # (N x B)
+        # [[MAD_{a}^{(-i)}[b]]_{b = 1}^{B}]_{i = 1}^{N}]
+        MAD_residuals = ResidualsCollection[NumDemos, NumBins].full(
+            N, Residuals[NumBins]()
+        )
 
         # (N x T_)
         z_scores = ZScoresCollection[NumDemos, NumPoints].full(N, ZScores[NumPoints]())
 
         for bin in self.bins:
-            for j in self.demonstrations.demo_indices:
-                loo_samples = bin.samples(LOO_demo_index=j)
+            for i in self.demonstrations.demo_indices:
+                loo_samples = bin.samples(LOO_demo_index=i)
                 loo_stats = self.compute_robust_consensus_statistics(loo_samples)
-                bin_median_action = loo_stats.median_action  # alpha_a^{(-j)}[b]
+                bin_median_action = loo_stats.median_action  # alpha_a^{(-i)}[b]
 
-                demo_samples = bin.samples_collection[j]
+                demo_samples = bin.samples_collection[i]
                 for action in demo_samples.actions():
                     residual = Residual(
                         la.norm(action - bin_median_action)
                     )  # r^{-i}_{i, t}
-                    self_action_residuals[j].append(residual)
+                    self_action_residuals[i].append(residual)
 
                 bin_action_residuals = list[Residual]()  # LOO
-                for action in bin.actions(LOO_demo_index=j):
+                for action in bin.actions(LOO_demo_index=i):
                     residual = Residual(
                         la.norm(action - bin_median_action)
-                    )  # r^{-j}_{i, t}
+                    )  # r^{-i}_{j, u} OR r^{-i}_{m, v}
                     bin_action_residuals.append(residual)
 
                 bin_median_action_residual = Residual(median(bin_action_residuals))
@@ -653,12 +656,16 @@ class PACER(Generic[NumBins, NumDemos, NumPoints, DimState, DimAction]):
                 for residual in bin_action_residuals:
                     abs_deviation = Residual(abs(residual - bin_median_action_residual))
                     abs_deviations.append(abs_deviation)
-                MAD_residual = Residual(MAD_SCALE * median(abs_deviations))
-                MAD_residuals.append(MAD_residual)
+                MAD_residual = Residual(
+                    MAD_SCALE * median(abs_deviations)
+                )  # MAD_{a}^{(-i)}[b]
+                MAD_residuals[i].append(MAD_residual)
 
         for i in self.demonstrations.demo_indices:
-            denom = MAD_residuals[i] + EPS
             for t in self.demonstrations.demos[i].time_indices:
+                sample_idx = SampleIndex(i, t)
+                bin_idx = self.sample_index_to_bin_index(sample_idx)
+                denom = MAD_residuals[i][bin_idx] + EPS
                 z_score = (self_action_residuals[i][t]) / denom  # z_{i, t}
                 z_scores[i].append(z_score)
 
