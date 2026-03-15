1ed287885478878e6a9b54278f4ab38da9f6618e
Ashrith Sagar <ashrithy@iisc.ac.in>
2026-03-15 17:06:54 +0530

    ♻️ PLY:pacer;
    Unified to test_pacer_bc;

--- playground/PACER/tests/test_pacer_bc.py
+++ playground/PACER/tests/test_pacer_bc.py
@@ -1,13 +1,17 @@
-# tests/test_lasa.py
+# tests/test_pacer_bc.py
 
-from typing import Any
+# pyright: standard
 
+from typing import Any, Literal
+
+import matplotlib.pyplot as plt
 from typingkit.core import RuntimeOptions, set_global_default_runtime_options
 from typingkit.numpy._typed.helpers import TWO
 
 from pacer import console
 from pacer.base import Demonstrations
 from pacer.corruptions import DemonstrationCorrupter
+from pacer.interactive import InteractiveDataSet
 from pacer.lasa import LASADataSet
 from pacer.plotting import full_diagnostic
 from pacer.trainers import BCTrainer, PACERBCTrainer
@@ -16,8 +20,26 @@ from pacer.typings import NumDemos, NumPoints
 set_global_default_runtime_options(RuntimeOptions(validate=True))
 
 
-def get_demonstrations() -> Demonstrations[Any, Any, TWO, TWO]:
-    return LASADataSet("GShape").to_demonstrations()
+def get_demonstrations(
+    choice: Literal[
+        "FROM_LASA", "CUSTOM_FROM_LOAD", "CUSTOM_FROM_LASA", "CUSTOM_DRAW"
+    ] = "FROM_LASA",
+) -> Demonstrations[Any, Any, TWO, TWO]:
+    match choice:
+        case "FROM_LASA":
+            return LASADataSet("GShape").to_demonstrations()
+        case "CUSTOM_FROM_LOAD":
+            drawer = InteractiveDataSet.load("hand_drawn_demos.npz")
+            return drawer.to_demonstrations()
+        case "CUSTOM_FROM_LASA":
+            drawer = InteractiveDataSet.from_LASA("GShape")
+            plt.show(block=True)
+            return drawer.to_demonstrations()
+        case "CUSTOM_DRAW":
+            drawer = InteractiveDataSet()
+            plt.show(block=True)
+            return drawer.to_demonstrations()
+    raise ValueError
 
 
 def test_pacerbc(
