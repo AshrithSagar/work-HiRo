da93c58ae8747a2a7a70dc72c7cca43529ca3c26
Ashrith Sagar <ashrithy@iisc.ac.in>
2026-07-05 01:15:15 +0530

    refactor(pacer): remove imitation.base

--- PACER/src/pacer/imitation/__init__.py
+++ PACER/src/pacer/imitation/__init__.py
@@ -3,12 +3,3 @@ Imitation Learning
 =======
 """
 # src/pacer/imitation/__init__.py
-
-from pacer.imitation.base import ILDataset, ILPolicy, ILTrainConfig, ILTrainer
-
-__all__ = [
-    "ILDataset",
-    "ILPolicy",
-    "ILTrainConfig",
-    "ILTrainer",
-]
